81616199e46dbf166b13366f2d97d8a1a4da2fb6
TheJoshuaEvans <thejoshuaevans@gmail.com>
2022-01-03 10:27:23 -0800

    Minor formatting improvement

--- frontend/src/01_introduction/Nested.svelte
+++ frontend/src/01_introduction/Nested.svelte
@@ -1,6 +1,6 @@
 <script>
   let paragraph = `This is another paragraph, but it looks different and contains
-  <strong>HTML</strong>. Please continue observing the following important image`;
+    <strong>HTML</strong>. Please continue observing the following important image`;
 </script>
 
 <p>{@html paragraph}</p>
