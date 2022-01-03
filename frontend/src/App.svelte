052719f2e5c7ce898072be1459e43675e1e83e15
TheJoshuaEvans <thejoshuaevans@gmail.com>
2022-01-03 02:34:23 -0800

    Introduction - Completed Svelte tutorial Introduction

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,1 +1,7 @@
-<h1>Hello world!</h1>
+<script>
+  import Introduction from './introduction/Introduction.svelte';
+</script>
+
+<h1>Svelte Practice</h1>
+<h2>Introduction</h2>
+<Introduction/>
---
+++ frontend/src/introduction/Introduction.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Nested from './Nested.svelte';
+
+  let name = 'world';
+  let src = '/extremely-important-image.gif'
+  let imgAltText = 'A dancing man. You could say he\'s really ROLLING with it';
+</script>
+
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 1.2em;
+	}
+</style>
+
+<p>Hello {name.toUpperCase()}!</p>
+
+<p>This is a paragraph. Please observe the following extremely important image </p>
+<Nested/>
+<img {src} alt="{imgAltText}" title="{imgAltText}">
